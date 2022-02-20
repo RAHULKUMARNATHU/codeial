e8478954a2d3f5be0beccb96ccfd1fe1d07934ff
Rahul <rahulvaidya148@gmail.com>
2022-02-21 02:15:23 +0530

    Created a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,10 @@
 module.exports.home = function(req , res){
-    return res.end('<h1>Express is up for Codeial!! </h1>')
+
+
+    return res.render('home' , {
+        title :"Home"
+    });
+    
+    // return res.end('<h1>Express is up for Codeial!! </h1>')
 }
 
--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+//set up the view engine
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+
+
 
 //listening here
 
