d45319c8dddc7d396fc832aa5a412f09da4297dd
Rahul <rahulvaidya148@gmail.com>
2022-02-21 01:52:21 +0530

    created another router as user_controller and routes then in users from index of routers

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
 module.exports = router;
