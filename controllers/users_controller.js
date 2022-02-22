9f5b4486254d1b6d7bb3d2b067c956a58a8e04da
Rahul <rahulvaidya148@gmail.com>
2022-02-22 17:52:24 +0530

    commited to work with header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,12 @@
-module.exports.profile = function(req ,res){
-    res.end('<h1>User Profile </h1>');
+// module.exports.profile = function(req ,res){
+//     // res.end('<h1>User Profile </h1>');
 
+//     return res.render('./user_profile' , {
+//         title :"User_Profile"
+//     });
+// }
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title : 'User Profile'
+    })
 }
