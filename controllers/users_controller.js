ba161ee272477bc42ca40291378b4f7c7506567f
Rahul <rahulvaidya148@gmail.com>
2022-02-24 23:58:47 +0530

    ceated sign in  form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,24 +5,39 @@
 //         title :"User_Profile"
 //     });
 // }
+
+// render profile page 
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title : 'User Profile'
     });
 }
 
-
+// render sign up page
 module.exports.signUp = function(req , res){
     return res.render('user_sign_up' , {
     title : 'Codial | Sign Up'
     });
-    
+
 }
 
-
+// render sign in page
 module.exports.signIn = function(req , res){
     return res.render('user_sign_in' , {
         title: 'Codial | Sign In'
     });
 
+}
+
+
+
+//get sing up data
+module.exports.create =function(req ,res){
+//TODO later
+
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req ,res){
+    // TODO later
 }
