95c9a677ee67014ea8421a8f5947614dfd1f08a2
Rahul <rahulvaidya148@gmail.com>
2022-02-24 22:42:50 +0530

    finally worked with routers also

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile' , usersController.profile);
-
-
+router.get('/sign-up' , usersController.signUp);
+router.get('/sign-in' , usersController.signIn);
 module.exports = router;
