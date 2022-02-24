dde6c6de2c6050bf1ebd5813e412d0290d4343b2
Rahul <rahulvaidya148@gmail.com>
2022-02-24 22:36:43 +0530

    Added sign_in and sign_up file and controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,5 +8,21 @@
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title : 'User Profile'
-    })
+    });
+}
+
+
+module.exports.signUp = function(req , res){
+    return res.render('user_sign_up' , {
+    title : 'Codial | Sign Up'
+    });
+    
+}
+
+
+module.exports.signIn = function(req , res){
+    return res.render('user_sign_in' , {
+        title: 'Codial | Sign In'
+    });
+
 }
