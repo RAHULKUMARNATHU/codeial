d32862d9d8d67dcd8d0b60d7e469bef9dae54da8
Rahul <rahulvaidya148@gmail.com>
2022-02-28 07:35:13 +0530

    siging in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -56,6 +56,64 @@ module.exports.create = function(req, res){
 }
 
 // sign in and create a session for the user
-module.exports.createSession = function(req ,res){
-    // TODO later
+// module.exports.createSession = function(req ,res){
+//     // Steps to authenticate
+//     //find the user
+//     User.findOne({email : req.body.email} , function(err,user){
+//         if(err){
+//             console.log('error in finding signing in');return}
+//         //handle user found
+//         if(user){
+//             //handle password which doesn't match
+//             if(user.password != req.body.password){
+//                 return res.redirect('back');
+//             }
+            
+//             //handle session creation 
+//             res.cookie('user_id',user.id);
+//             return res.redirect('/users/profile');
+
+
+//         }else{
+//             // handle user not found
+//             return res.redirect('back');
+//         }
+
+
+//     });
+// }
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+        if (user){
+
+            // handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+
+            return res.redirect('back');
+        }
+
+
+    });
+
+ 
+
+    
+
+    
 }
