b0779c9712235d46b9d2345a361ee364b1e39fa2
Rahul <rahulvaidya148@gmail.com>
2022-02-28 17:28:36 +0530

    just try to make change in master and want to see reflect in manual-local-auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -105,36 +105,36 @@ module.exports.create = function(req, res){
 // }
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
+// module.exports.createSession = function(req, res){
 
-    // steps to authenticate
-    // find the user
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing in'); return}
-        // handle user found
-        if (user){
+//     // steps to authenticate
+//     // find the user
+//     User.findOne({email: req.body.email}, function(err, user){
+//         if(err){console.log('error in finding user in signing in'); return}
+//         // handle user found
+//         if (user){
 
-            // handle password which doesn't match
-            if (user.password != req.body.password){
-                return res.redirect('back');
-            }
+//             // handle password which doesn't match
+//             if (user.password != req.body.password){
+//                 return res.redirect('back');
+//             }
 
-            // handle session creation
-            res.cookie('user_id', user.id);
-            return res.redirect('/users/profile');
+//             // handle session creation
+//             res.cookie('user_id', user.id);
+//             return res.redirect('/users/profile');
 
-        }else{
-            // handle user not found
+//         }else{
+//             // handle user not found
 
-            return res.redirect('back');
-        }
+//             return res.redirect('back');
+//         }
 
 
-    });
+//     });
 
  
 
     
 
     
-}
+// }
