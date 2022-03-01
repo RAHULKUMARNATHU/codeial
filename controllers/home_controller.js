126f04c2982ba62e1b7ca809a44eb1bcaeedca3a
Rahul <rahulvaidya148@gmail.com>
2022-03-01 12:08:12 +0530

    Epress sessions and using passport

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,12 @@
 module.exports.home = function(req , res){
 
     console.log(req.cookies)
-    res.cookie("user_id",11)
+    // res.cookie("user_id",12)
     return res.render('home' , {
         title :"Home"
+   
     });
-    
+   
     // return res.end('<h1>Express is up for Codeial!! </h1>')
 }
 
