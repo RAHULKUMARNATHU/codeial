9a24710e02bf822290a60da4f5c8968856928a05
Rahul <rahulvaidya148@gmail.com>
2022-03-03 20:34:48 +0530

    Display Post and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,29 @@
+const Post = require('../models/post');
+
+
 module.exports.home = function(req , res){
 
-    console.log(req.cookies)
+    // console.log(req.cookies)
     // res.cookie("user_id",12)
+   
+//    Post.find({},function(err ,posts){
+//     return res.render('home' , {
+//         title :"Codeil | Home ",
+//         posts: posts
+   
+//     });
+   
+//    });
+
+// populate the user of each post
+   Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home' , {
-        title :"Home"
+        title :"Codeil | Home ",
+        posts: posts
    
-    });
-   
+    }); 
+   })
+
     // return res.end('<h1>Express is up for Codeial!! </h1>')
 }
 
