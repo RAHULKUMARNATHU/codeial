41fd46d870f322f002fdfe4a0340b8baec53ee85
Rahul <rahulvaidya148@gmail.com>
2022-03-04 15:56:35 +0530

    Adding Comments to database

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ console.log('router loaded');
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 
 module.exports = router;
