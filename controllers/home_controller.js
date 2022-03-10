ac51c75498c82dd4f7d959908db7cfd8125f0372
Rahul <rahulvaidya148@gmail.com>
2022-03-10 14:04:38 +0530

    User profile-links and logged user can delete the comment if  post is created by that user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
     // console.log(req.cookies);
@@ -22,12 +23,17 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+
+        User.find({} , function(err ,users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts ,
+                all_users: users
+
+            });
+
         });
     })
-
 }
 
 // module.exports.actionName = function(req, res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,10 +13,17 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title : 'User Profile'
+   
+    User.findById(req.params.id , function(err , user ){
+        return res.render('user_profile', {
+            title : 'User Profile',
+            profile_user:user
+        });
     });
+    
 }
+
+
 //     if(req.cookies.user_id){
 //         User.findById(req.cookies.user_id,function(err, user){
 //             if(user){
