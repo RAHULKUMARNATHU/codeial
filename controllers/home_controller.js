0874e0a37c8e43ff9255754912d29365eec7ae4c
Rahul <rahulvaidya148@gmail.com>
2022-03-11 06:32:29 +0530

    converting asyn await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
+//module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
@@ -14,26 +14,31 @@ module.exports.home = function(req, res){
     // });
 
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+module.exports.home = async function(req, res){
 
-        User.find({} , function(err ,users){
+        try{
+             // populate the user of each post
+            let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+        
+            let users = await User.find({});
+    
             return res.render('home', {
                 title: "Codeial | Home",
-                posts:  posts ,
+                posts:  posts,
                 all_users: users
-
             });
-
-        });
-    })
-}
-
+    
+        }catch(err){
+            console.log('Error', err);
+            return;
+        }
+       
+    }
 // module.exports.actionName = function(req, res){}
