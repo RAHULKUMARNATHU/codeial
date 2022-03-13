48ccfcfdbeb8e079a37e47c87c099154fb319e90
Rahul <rahulvaidya148@gmail.com>
2022-03-13 22:52:42 +0530

    Saving Files from Params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -15,16 +15,54 @@ module.exports.profile = function(req, res){
 }
 
 
-module.exports.update = function(req ,res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id , req.body , function(err , user){
-            return res.redirect('back');
-        });
-    }else{
-        return res.status(401).send('Unauthorized');
-    }
-}
+// module.exports.update =function(req ,res){
+//     if(req.user.id == req.params.id){
+//         User.findByIdAndUpdate(req.params.id , req.body , function(err , user){
+//             return res.redirect('back');
+//         });
+//     }else{
+//         return res.status(401).send('Unauthorized');
+//     }
+// }
 
+module.exports.update = async function(req ,res){
+        if(req.user.id == req.params.id){
+            try{
+                
+                let user = await User.findById(req.params.id);
+                User.uploadedAvatar(req , res , function(err){
+                    if(err){
+                        console.log('****Multer Error',err)
+                    }
+                    // console.log(req.file);
+                    
+                    user.name = req.body.name;
+                    user.email = req.body.email;
+
+                    if(req.file){
+                        // this is saving the path of the uploaded file into the avatar field int he user
+                        user.avatar = User.avatarPath + '/' + req.file.filename 
+                    }
+                    user.save();
+                    return res.redirect('back');
+               
+                });
+
+            }catch(err){
+                req.flash('error' , err);
+                return res.redirect('back');
+            }
+
+
+
+        }else{
+            req.flash('error','unauthorized')
+            return res.status(401).send('Unauthorized');
+            }
+
+
+
+}
 
 // render sign up page
 module.exports.signUp = function(req , res){
--- models/user.js
+++ models/user.js
@@ -31,13 +31,21 @@ const userSchema = new mongoose.Schema({
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,'..',AVATAR_PATH))
+      cb(null, path.join(__dirname,'..', AVATAR_PATH))
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    // cb(null, file.fieldname + '-' + Date.now())
+      let uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
-  })
+  });
+
+// static methods
+userSchema.statics.uploadedAvatar = multer({storage:storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+
+
 
 const User = mongoose.model('User', userSchema);
 
