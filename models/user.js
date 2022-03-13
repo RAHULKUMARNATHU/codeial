9971b1c3c296593ab8368cfafd2b131b11ec72d5
Rahul <rahulvaidya148@gmail.com>
2022-03-13 15:33:08 +0530

    installing multer+ Documentation

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const AWATAR_PATH = path.join('/uploads/users/awatars');
+
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
