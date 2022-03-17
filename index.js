0b4e18fb6afabfa43114e1596a6b84d90038635d
Rahul <rahulvaidya148@gmail.com>
2022-03-18 00:46:16 +0530

    Using Google Auth ::With a Link

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy')
+
 // const MongoStore = require('connect-mongo')(session);
 
 const MongoStore = require('connect-mongo')(session);
--- routes/users.js
+++ routes/users.js
@@ -26,4 +26,8 @@ router.post('/create-session' , passport.authenticate(
 
 router.get('/sign-out',usersController.destroySession);
 
+router.get('/auth/google',passport.authenticate('google' , {scope: ['profile' , 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google',{failureRedirect: '/users/sign-in'}), usersController.createSession);
+
+
 module.exports = router;
