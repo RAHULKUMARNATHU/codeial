9bf1718b78665d156c38955dd21bdd05500b098a
Rahul <rahulvaidya148@gmail.com>
2022-03-19 15:14:39 +0530

    Setting up Nodemailer and installed nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,40 @@
+// required libraries
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path =require('path');
+
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    host : 'smtp.gmail.com',
+    port :587,
+    secure:false,
+    auth:{
+        user:'rahulvaidya148@gmail.com',
+        pass:'7669Shiva@'
+    }
+});
+
+
+let renderTemplate = (data , relativePath) => {
+    let mailHTML ;
+    ejs.renderFile(
+        path.join(__dirname , '../views/mailers', relativePath),
+        data,
+        function(err , template){
+            if (err){
+                console.log('Error in rendering template');
+                return;
+
+            }
+            mailHTML = template;
+        }
+        
+    )
+    return mailHTML;
+}
+
+module.exports ={
+    transporter:transporter,
+    renderTemplate : renderTemplate
+
+}
